bc45efc57cee25a69fb0fdd62930d9a59d4cc1cf
Ayaz <asiddiq2@ncsu.edu>
2021-03-14 14:59:58 -0400

    added project startup script.

---
+++ projectStartup.m
@@ -0,0 +1,7 @@
+clear
+close all
+bdclose all
+format compact
+set(groot,'defaulttextinterpreter','latex');  
+set(groot, 'defaultAxesTickLabelInterpreter','latex');  
+set(groot, 'defaultLegendInterpreter','latex');
