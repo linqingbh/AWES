b19715b80b40f8f9315e421b5c65b06e76a86995
Ayaz <asiddiq2@ncsu.edu>
2021-03-15 18:35:30 -0400

    these are not the plots that Licitra has.

--- simulink/libraries/aerodynamics/calculateAeroCoeff.m
+++ simulink/libraries/aerodynamics/calculateAeroCoeff.m
@@ -1,12 +1,56 @@
-function [cXa,cXq,cXde,cX0] = calculateAeroCoeff(a)
+function [CX,CY,CZ,CL,CM,CN] = calculateAeroCoeff(a,b,da,de,dr,pH,qH,rH)
 
 pF = @(c0,c1,c2,a) c2*a.^2 + c1*a + c0;
 
-cXa = pF(0,8.32,0,a);
-cXq = pF(-0.6,4.41,0,a);
+%% CX
+cXa  = pF(0,8.32,0,a);
+cXq  = pF(-0.6,4.41,0,a);
 cXde = pF(-0.01,0.11,0,a);
 cX0  = pF(0.46,0,0,a);
 
+CX   = cXa.*a + cXq.*qH + cXde.*de + cX0;
 
+%% CY
+cYb  = pF(-.19,0,0,a);
+cYp  = pF(-.10,0,0,a);
+cYr  = pF(0.17,0.14,0,a);
+cYda = pF(-.05,0,0,a);
+cYdr = pF(.1,0,0,a);
+
+CY   = cYb.*b + cYp.*pH + cYr.*rH + cYda.*da + cYdr.*dr;
+
+%% CZ
+cZa  = pF(0,1.23,10.2,a);
+cZq  = pF(-7.56,0.13,6.15,a);
+cZde = pF(-.32,0,0.29,a);
+cZ0  = pF(-5.4,0,0,a);
+
+CZ   = cZa.*a + cZq.*qH + cZde.*de + cZ0;
+
+%% CL
+cLb  = pF(-.06,0,0,a);
+cLp  = pF(-.56,0,0,a);
+cLr  = pF(.18,.65,0,a);
+cLda = pF(-.25,.04,0,a);
+cLdr = pF(0,0,0,a);
+
+CL   = cLb.*b + cLp.*pH + cLr.*rH + cLda.*da + cLdr.*dr;
+
+%% CM
+cMa  = pF(0,.21,0,a);
+cMq  = pF(-11.30,0,5.29,a);
+cMde = pF(-1.02,0,0,a);
+cM0  = pF(-.32,0,0,a);
+
+CM   = cMa.*a + cMq.*qH + cMde.*de + cM0;
+
+%% CN
+cNb  = pF(0.06,-.09,0,a);
+cNp  = pF(-.06,-.91,0,a);
+cNr  = pF(-.05,0,0,a);
+cNda = pF(.02,-.12,0,a);
+cNdr = pF(-.04,0,0,a);
+
+CN   = cNb.*b + cNp.*pH + cNr.*rH + cNda.*da + cNdr.*dr;
 
 end
--- simulink/libraries/aerodynamics/test.m
+++ simulink/libraries/aerodynamics/test.m
@@ -1,5 +1,52 @@
 clear
+close all
 
-a = linspace(-5,17,101);
+a = linspace(-5,17,51);
+b = 0;
+da = 0;
+de = 0;
+dr = 0;
+pH = 0;
+qH = 0;
+rH = 0;
 
-[cXa,cXq,cXde,cX0] = calculateAeroCoeff(a*pi/180);
+WcB = @(a,b) [cos(a)*cos(b) sin(b) sin(a)*sin(b);...
+    -cos(a)*sin(b) cos(b) -sin(a)*sin(b);
+    -sin(a) 0 cos(a)];
+
+[CX,CY,CZ,CL,CM,CN] = calculateAeroCoeff(a(:)*pi/180,b,da,de,dr,pH,qH,rH);
+
+DCL = [0;0;0].*a;
+
+for ii = 1:length(a)
+    
+DCL(:,ii) = WcB(a(ii),0)*[CX(ii);CY(ii);CZ(ii)];
+
+end
+subplot(3,3,1); plot(a,CX); ylabel('CX');
+
+subplot(3,3,2); plot(a,CY); ylabel('CY');
+
+subplot(3,3,3); plot(a,CZ); ylabel('CZ');
+
+subplot(3,3,4); plot(a,CL); ylabel('CL');
+
+subplot(3,3,5); plot(a,CM); ylabel('CM');
+
+subplot(3,3,6); plot(a,CN); ylabel('CN');
+
+subplot(3,3,7); plot(a,DCL(1,:)); ylabel('D');
+
+subplot(3,3,8); plot(a,DCL(2,:)); ylabel('C');
+
+subplot(3,3,9); plot(a,DCL(3,:)); ylabel('L');
+
+figure
+plot(a,DCL(3,:)./DCL(1,:)); ylabel('L/D');
+
+
+allAxes = findall(0,'type','axes');
+set(allAxes,'FontSize',9);
+xlabel(allAxes,'Time [s]');
+grid(allAxes,'on');
+xlim(allAxes,[min(a) max(a)]);
